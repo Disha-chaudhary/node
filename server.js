038faf6974256b68db655f06589b5297e6816642
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-01-10 15:03:51 +0530

    create server

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const http = require('http');
-http.createServer().listen(5000);
+http.createServer((req,resp)=>{
+    resp.write("hello this is node js server\n");
+    resp.end("hello");
+}).listen(5000);
