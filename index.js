575ded6d6e13ed6cbf1d99992305124a0f5df25b
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-01-16 14:47:03 +0530

    reading a file

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 // var colors = require('colors');
 // console.log("Hello, World!".green);
 // console.log("Dishaa Chaudharyyyyyyy");
-const fs = require("fs");
-const fh = fs.openSync("Secfb","w");
-// console.log(fh);
+// const fs = require("fs");
+// const fh = fs.openSync("Secfb","w");
 
-fs.writeFileSync(fh,"we are here to study");
-console.log(fs.readFileSync("Secfb","utf8"));
+
+// fs.writeFileSync(fh,"we are here to study");
+// console.log(fs.readFileSync("Secfb","utf8"));
+//fs.close(fh)
+const fs = require("fs");
+const fh = fs.openSync("file1","r");
+
+console.log(fs.readFileSync("file1","utf8"));
+
