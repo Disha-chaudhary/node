6b9c876b7f05dade90303f9d50d5bb55542085f4
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-01-16 15:29:49 +0530

    fileSystem

--- index.js
+++ index.js
@@ -9,8 +9,15 @@
 // fs.writeFileSync(fh,"we are here to study");
 // console.log(fs.readFileSync("Secfb","utf8"));
 //fs.close(fh)
-const fs = require("fs");
-const fh = fs.openSync("file1","r");
+// const fs = require("fs");
+// const fh = fs.openSync("file1","r");
 
-console.log(fs.readFileSync("file1","utf8"));
+// console.log(fs.readFileSync("file1","utf8"));
+const fs = require("fs");
+const fh = fs.openSync("file1","w");
+// console.log(fs.appendFileSync("file1 "," hello my name is disha chaudhary"));
+
+fs.renameSync("file1","file2");
+fs.cpSync("secfb","file1");
+fs.unlinkSync("file2");
 
