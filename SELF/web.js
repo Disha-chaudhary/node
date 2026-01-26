515718fd17134f2f35e230d8a8dd58f23e792d85
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-01-26 22:02:59 +0530

    passing html

--- SELF/web.js
+++ SELF/web.js
@@ -2,6 +2,13 @@ const http = require('http');
 const fs = require('fs');
 http.createServer((req,resp)=>{
     fs.readFile("SELF/html/web.html", "utf8", (err, data)=>{
-        
+        if(err){
+            resp.write("InternalServer error");
+            resp.end();
+            return
+        }
+        resp.writeHead(200,{"content-type":'text/html'});
+        resp.write(data);
+        resp.end();
     });
-}).listen(5000);
+}).listen(5100);
