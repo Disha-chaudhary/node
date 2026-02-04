e902d26f27eb4e80c524d396c2df8da9d5eee728
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-02-04 12:36:27 +0530

    practicce

--- MAIN/index.js
+++ MAIN/index.js
@@ -3,7 +3,34 @@ const fs = require("fs");
 const url = require("url");
 
 http.createServer((req,resp)=>{
-    resp.write("<h1>hello!!!</h1>");
-    resp.end();
+    // resp.write("<h1>hello!!!</h1>");
+    // resp.end();
+    switch(req.url){
+        case '/': fs.readFile('./Components/home.html',(err,page)=>{
+            if(err){
+                console.log(err);
+            resp.end();           
+         }
+         else{
+            resp.write("welcome to home page");
+            resp.write(page);
+            resp.end();
+         }
+        
+           });
+           break;
+        case '/about':fs.readFile('./Components/about.html',(err,page)=>{
+            if(err){
+                console.log(err);
+                resp.end();
+            }
+            else{
+                resp.write("welcome to about page");
+                resp.write(page);
+                resp.end();
+            }
+        });
+        break;
+    }
 
-}).listen(5600);
+}).listen(5300);
