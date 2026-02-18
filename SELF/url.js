f7c12f3163394ee81a4b06683d11a3268253451b
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-02-18 20:30:01 +0530

    accessing categories

--- SELF/url.js
+++ SELF/url.js
@@ -5,3 +5,10 @@ let address = 'https://www.example.com/category/search?name=disha&age=21#section
 const myURL = url.parse(address, true);
  
 console.log(myURL);
+
+console.log(myURL.protocol); // https:
+console.log(myURL.hostname); // www.example.com
+console.log(myURL.pathname); // /category/search
+console.log(myURL.search);   
+console.log(myURL.query);    
+console.log(myURL.hash);     // #section4
