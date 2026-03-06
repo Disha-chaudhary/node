4643805be363feb42ff460a51770d8913c3b3303
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-03-06 15:45:10 +0530

    sending html

--- SELF/midtermPrep/response.js
+++ SELF/midtermPrep/response.js
@@ -1,5 +1,30 @@
 const http = require('http');
 
 http.createServer((req,resp)=>{
-    resp.end('<h1>disha Chaudhary</h1>')
-}).listen(5000);
+ //What is setHeader() in Node.js?
+//setHeader() is a method of the HTTP response object (res)
+//  used to set HTTP response headers before sending the response to the client (browser).
+//Headers tell the browser what type of data the server is sending.
+resp.setHeader('Content-Type','text/html');
+    // resp.end('<h2>dishaaaaa Chaudhary</h2>');
+    resp.write(`
+        <html>
+        <head>
+        <style>
+body{
+background-color: lightblue;
+text-align:center;
+}
+
+h1{
+color:red;
+}
+
+</style>
+        <body>
+        <h1>Hello my name is disha</h1>
+        <p>welcome to my node js server</p>
+        </body>
+        </html>`)
+        resp.end();
+}).listen(6500);
