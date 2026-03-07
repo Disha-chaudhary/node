f5247d28a6094eb4761f316fda2f7eabf042d88e
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-03-08 01:23:38 +0530

    formmm

--- SELF/form.js
+++ SELF/form.js
@@ -1,24 +1,57 @@
 const http = require('http');
+const fs = require('fs');
 
-http.createServer((req, resp) => {
+http.createServer((req,resp)=>{
 
-    if (req.url === "/") {
-        resp.writeHead(200, { "Content-Type": "text/html" });
-        resp.write(`
-            <form action="/submit" method="post">
-                <input type="text" placeholder="enter name" name="name"><br><br>
-                <input type="text" placeholder="enter email" name="email"><br><br>
-                <button type="submit">Submit!!</button>
-            </form>
-        `);
-        resp.end();
+    if(req.url=='/') {
+
+        fs.readFile('html/form.html','utf-8',(error,data)=>{
+
+            if(error){
+                resp.writeHead(500,{'content-Type':"text/plain"});
+                resp.end("Internal Server Error");
+                return;
+            }
+
+            resp.writeHead(200,{"content-type":"text/html"});
+            resp.write(data);
+            resp.end();
+
+        });
+
     }
 
-    else if (req.url === "/submit") {
-        resp.writeHead(200, { "Content-Type": "text/html" });
-        resp.end("<h2>Form submitted successfully 👍</h2>");
+    else if(req.url=="/submit") {
+
+        resp.writeHead(200,{"content-type":"text/html"});
+        resp.end("<h1>Form Submitted Successfully</h1>");
+
     }
 
-}).listen(3200, () => {
-    console.log("Server running at http://localhost:3200");
-});
+}).listen(4070);
+
+console.log("Server running on http://localhost:3900");
+
+// http.createServer((req, resp) => {
+
+//     if (req.url === "/") {
+//         resp.writeHead(200, { "Content-Type": "text/html" });
+//         resp.write(`
+//             <form action="/submit" method="post">
+//             <h1>Form<h1>
+//                 <input type="text" placeholder="enter name" name="name"><br><br>
+//                 <input type="text" placeholder="enter email" name="email"><br><br>
+//                 <button type="submit">Submit here !!</button>
+//             </form>
+//         `);
+//         resp.end();
+//     }
+
+//     else if (req.url === "/submit") {
+//         resp.writeHead(200, { "Content-Type": "text/html" });
+//         resp.end("<h2>Form submitted successfully 👍</h2>");
+//     }
+
+// }).listen(3600, () => {
+//     console.log("Server running at http://localhost:3200");
+// });
--- SELF/midtermPrep/response.js
+++ SELF/midtermPrep/response.js
@@ -1,30 +1,33 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer((req,resp)=>{
- //What is setHeader() in Node.js?
-//setHeader() is a method of the HTTP response object (res)
-//  used to set HTTP response headers before sending the response to the client (browser).
-//Headers tell the browser what type of data the server is sending.
-resp.setHeader('Content-Type','text/html');
+http
+  .createServer((req, resp) => {
+    //What is setHeader() in Node.js?
+    //setHeader() is a method of the HTTP response object (res)
+    //  used to set HTTP response headers before sending the response to the client (browser).
+    //Headers tell the browser what type of data the server is sending.
+    resp.setHeader("Content-Type", "text/html");
     // resp.end('<h2>dishaaaaa Chaudhary</h2>');
     resp.write(`
         <html>
         <head>
         <style>
-body{
-background-color: lightblue;
-text-align:center;
-}
+        
+    body{
+        background-color: lightblue;
+        text-align:center;
+    }
 
-h1{
-color:red;
-}
+    h1{
+    color:red;
+    }
 
 </style>
         <body>
         <h1>Hello my name is disha</h1>
         <p>welcome to my node js server</p>
         </body>
-        </html>`)
-        resp.end();
-}).listen(6500);
+        </html>`);
+    resp.end();
+  })
+  .listen(6500);
